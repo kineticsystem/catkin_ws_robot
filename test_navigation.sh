e8ea752829ee8ac3099870746ac8b4aed8a70972
Giovanni Remigi <g.remigi@kineticsystem.org>
2021-06-17 14:50:14 +0100

    Added test_navigation script

---
+++ test_navigation.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T "Gazebo" -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T "AMCL" -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+xterm -xrm 'XTerm.vt100.allowTitleOps: false' -T "RVIZ" -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
